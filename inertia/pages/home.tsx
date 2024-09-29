30dc5f35795acfeb7671702cfc8835fbaa94f574
HugoLaurent <lauhugopro@gmail.com>
2024-09-29 14:10:36 +0200

    Refactor: Update Header component and styles

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -1,11 +1,12 @@
 import { Head } from '@inertiajs/react'
+import Hero from '~/components/Hero/Hero'
 import LayoutUser from '~/Layout/LayoutUser'
 
 export default function Home() {
   return (
     <LayoutUser>
       <Head title="Homepage" />
-      Coucou
+      <Hero />
     </LayoutUser>
   )
 }
