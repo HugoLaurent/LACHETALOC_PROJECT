bf16092e83146c0bea5556d5196039d3884e90b6
HugoLaurent <lauhugopro@gmail.com>
2024-09-29 12:50:33 +0200

    Add: Ajout d'un LayoutUser pour le composant Header

---
+++ inertia/Layout/LayoutUser.tsx
@@ -0,0 +1,8 @@
+export default function LayoutUser({ children }: any) {
+  return (
+    <div>
+      <p>layoutUser</p>
+      <div>{children}</div>
+    </div>
+  )
+}
--- start/routes.ts
+++ start/routes.ts
@@ -1,25 +1,19 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
-
 import AutoSwagger from 'adonis-autoswagger'
 import swagger from '#config/swagger'
-// returns swagger in YAML
+
+// Returns swagger in YAML with the custom route `/` included
 router.get('/swagger', async () => {
+  // Ajouter manuellement la route /
+
+  // Retourne la documentation Swagger
   return AutoSwagger.default.docs(router.toJSON(), swagger)
 })
-router.on('/').renderInertia('home')
 
 // Renders Swagger-UI and passes YAML-output of /swagger
 router.get('/docs', async () => {
   return AutoSwagger.default.ui('/swagger', swagger)
-  // return AutoSwagger.default.scalar("/swagger"); to use Scalar instead
-  // return AutoSwagger.default.rapidoc("/swagger", "view"); to use RapiDoc instead (pass "view" default, or "read" to change the render-style)
 })
+
+// PUBLIC ROUTES
+router.on('/').renderInertia('Home')
