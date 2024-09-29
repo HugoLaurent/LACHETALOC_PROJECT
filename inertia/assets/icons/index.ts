c46308ee3116b55cb87097ae191d978e4ce76157
HugoLaurent <lauhugopro@gmail.com>
2024-09-29 15:00:37 +0200

    Add: Ajout des icônes pour les types de logement

---
+++ inertia/assets/icons/index.ts
@@ -0,0 +1,19 @@
+import appartementIcon from './key-set.png'
+import banlieuIcon from './banlieu.png'
+import campagneIcon from './campagne.png'
+import cityCenterIcon from './paysage-urbain.png'
+import collocationIcon from './appartement.png'
+import maisonIcon from './maison.png'
+import studioIcon from './studio.png'
+import timeIcon from './time.png'
+
+export {
+  appartementIcon,
+  banlieuIcon,
+  campagneIcon,
+  cityCenterIcon,
+  collocationIcon,
+  maisonIcon,
+  studioIcon,
+  timeIcon,
+}
